5e59d6e584a44cb3dc6a681b53e14d9b6de3825c
joderrick1022 <joderrick1022>
2017-11-07 11:29:24 -0500

    paractice

---
+++ Scripts/JavaScript.js
@@ -0,0 +1,61 @@
+﻿
+//var name = "JD";
+
+
+
+//for (i = 0; i < 11; i++)
+//{
+
+//    document.write(name);
+
+//    for (var j = 1; j <= 10; j++)
+//    {
+
+//        if(i = j)
+//            {
+//            document.write("" + "is");
+//            break
+//        }
+        
+        
+//    }
+//}
+
+
+
+function helloWorld()
+{
+    var greeting = "hello World";
+
+    console.log(greeting);
+}
+helloWorld();
+
+
+
+function add(a, b)
+{
+    return a + b;
+}
+
+document.write(add(5, "fish"));
+
+
+var otherFunction = function(a,b){
+    return a + b;
+
+}
+console.log(otherFunction(2, 4));
+
+//write a function that takes in a name and print "hello name"
+
+
+
+function sayHello(name) {
+    
+    
+    return "hello" + name;
+}
+
+alert(sayHello("kfc"));
+
